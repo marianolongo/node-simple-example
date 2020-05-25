b285b7dd96dd83eb4503494071f665982d612197
Mariano Longo <mariano.longo@ing.austral.edu.ar>
2020-05-25 00:14:44 -0300

    added 100k instead of 10k

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -4,7 +4,7 @@ export async function myFunction(req: Request, res: Response) {
 
     let list: string[] = [];
 
-    for (let i = 0; i < 10000; i++) {
+    for (let i = 0; i < 100000; i++) {
         list.push("test")
     }
 
